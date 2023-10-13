4521c85b271707ffcfe34fa598a1b7f13b113778
xiaochu2 <ichuhuaqi@gmail.com>
2023-10-13 14:55:26 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+	  <web-view :webview-styles="webviewStyles" src="https://hightechex.vip"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {},
+		methods: {}
+	}
+</script>
+
+
+<style>
+	.webviewStyles{
+		height: 100vh !important;
+	}
+	.content {
+		height: 100vh !important;
+	}
+	iframe{
+		height: 100% !important;
+	}
+</style>
+
